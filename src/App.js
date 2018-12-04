1281d529669a9fa30d1220b6424ed5bce47dc828
unknown <juristein21@gmail.com>
2018-12-04 16:18:15 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Landing from './Components/Landing'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Landing/>
+      </div>
+    );
+  }
+}
+
+export default App;
