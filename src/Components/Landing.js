962179163f9c29b2fad0de12c8876624a41e173f
unknown <juristein21@gmail.com>
2018-12-04 18:49:36 +0100

    creating landing page

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './Landing.css'
 
 class Landing extends React.Component{
     render() {
         return (
             <div>
-                <p>test</p>
+                <img id="logo" alt="logo" src={require('../Assets/MYtineraryLogo.png')} />
+                <div className="text"><p>Find your perfect trip, desinged by insiders who know and love their cities.</p></div>
+                <h2>Start browsing</h2>
+                <img id="browse" alt="browse" src={require('../Assets/circled-right-2.png')}/>
+                <p>Want to build you own MYtinerary?</p>
+                <p className="links"><span><a href="">Log in</a></span>
+                <span><a href="">Create Account</a></span></p>
+                <div id="home">
+                <img alt="home" src={require('../Assets/homeIcon.png')}/>
+                </div>
             </div>
         );
     }
