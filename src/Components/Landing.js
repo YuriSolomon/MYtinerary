fb2b724d244495d64243fd81d69d9f3f9da0e51a
unknown <juristein21@gmail.com>
2018-12-05 17:03:55 +0100

    desing change - start

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -2,19 +2,28 @@ import React from 'react';
 import './Landing.css'
 
 class Landing extends React.Component{
+    getCity(props) {
+        var cities = ["New York","Berlin","Barcelona","Amsterdam","London","Paris"];
+        var city = cities[Math.floor(Math.random()*cities.length)];
+        props.ranCity =( <p>{city}</p> );
+    }
     render() {
         return (
             <div>
-                <img id="logo" alt="logo" src={require('../Assets/MYtineraryLogo.png')} />
-                <div className="text"><p>Find your perfect trip, desinged by insiders who know and love their cities.</p></div>
-                <h2>Start browsing</h2>
+                <a href="mailto:s.patel@ubiqum-mytinerary.com"><img id="logo" alt="logo" src={require('../Assets/MYtineraryLogo.png')} /></a>
+                <div className="text"><h5>Find your perfect trip, desinged by insiders who know and love their cities.</h5></div>
+                {/* <h2>Start browsing</h2> */}
                 <img id="browse" alt="browse" src={require('../Assets/circled-right-2.png')}/>
-                <p>Want to build you own MYtinerary?</p>
-                <p className="links"><span><a href="">Log in</a></span>
-                <span><a href="">Create Account</a></span></p>
-                <div id="home">
-                <img alt="home" src={require('../Assets/homeIcon.png')}/>
+                {/* <p>Want to build you own MYtinerary?</p> */}
+                {/* <p className="links"><span><a href="">Log in</a></span> */}
+                {/* <span><a href="">Create Account</a></span></p> */}
+                {/* <div id="home">
+                    <img alt="home" src={require('../Assets/homeIcon.png')}/>
+                </div> */}
+                <div className="locs">
+                    <h5>Popular MYtineraries</h5>
                 </div>
+                <div>{this.props.ranCity}</div>
             </div>
         );
     }
