8d751c3f48252cf600aa92513ec0c2c09b85458c
unknown <juristein21@gmail.com>
2018-12-10 17:48:03 +0100

    create router links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import Landing from './Components/Landing';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Login from './Components/Login';
+import SignUp from './Components/SignUp';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <BrowserRouter>
+          <div>
+            <Link to='/login'>login</Link>
+            <Link to='/signup'>signup</Link>
+            <Route path='/login' component={Login}/>
+            <Route path='/signup' component={SignUp}/>
+          </div>
+        </BrowserRouter>
         <Landing/>
       </div>
     );
---
+++ src/Components/Cities.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Cities extends React.Component{
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default Cities;
